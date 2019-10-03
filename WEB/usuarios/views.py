6e94a1ea883d759230bf0ead95bb1e491988ffb1
gimape07 <a01702312@itesm.mx>
2019-10-02 21:30:36 -0500

    get registered users and unregistered users

--- WEB/usuarios/views.py
+++ WEB/usuarios/views.py
@@ -8,7 +8,21 @@ from django.http import HttpResponse
 from usuarios.models import Usuario
 from usuarios.models import UsuariosRegistrados
 
+def get_non_registered_users():
+    users_r = UsuariosRegistrados.objects.all()
+    list_of_ids = []
+    for user_r in users_r:
+        list_of_ids.append(user_r.user_id)
+    return Usuario.objects.exclude(id_usuario__in=list_of_ids)
+
+def get_registered_users():
+    users_r = UsuariosRegistrados.objects.all()
+    list_of_ids = []
+    for user_r in users_r:
+        list_of_ids.append(user_r.user_id)
+    return Usuario.objects.filter(id_usuario__in=list_of_ids)
+
 def show_users(request):
-    users = Usuario.objects.all()
+
     args = {'title':'CECEQ USUARIOS', 'users':users}
     return render(request,'usuarios/usuarios.html', args)
