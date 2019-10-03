198893bbab0610be1cddf91472b3374c4c8ba27d
gimape07 <a01702312@itesm.mx>
2019-10-02 19:24:21 -0500

    last tests with the database model on html page

--- WEB/usuarios/views.py
+++ WEB/usuarios/views.py
@@ -9,6 +9,6 @@ from usuarios.models import Usuario
 from usuarios.models import UsuariosRegistrados
 
 def show_users(request):
-    users = Usuario.objects.filter(id_usuario=1)
-    print(users)
-    return render(request,'usuarios/usuarios.html',{'title': 'CECEQ USUARIOS'})
+    users = Usuario.objects.all()
+    args = {'title':'CECEQ USUARIOS', 'users':users}
+    return render(request,'usuarios/usuarios.html', args)
