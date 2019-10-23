8b4229a1503b237253f374f7b2c10009885ea6b9
Jose Carlos <a01702828@itesm.mx>
2019-10-23 17:37:09 -0500

    Descarga de repotes

--- WEB/reportes/urls.py
+++ WEB/reportes/urls.py
@@ -13,5 +13,15 @@ urlpatterns = [
     path('reporte-salones/', views.salones, name='re-salones'),
     path('reporte-usuarios/', views.show_users, name='re-usuarios'),
     path('reporte-vistas/', views.visitas, name='re-vistas'),
+    path('reporte-hoy/', views.reporte_usuarios_hoy, name='reporte-hoy'),
+    path('reporte-semana/', views.reporte_usuarios_semana, name='reporte-semana'),
+    path('reporte-año/', views.reporte_usuarios_año, name='reporte-anio'),
+    path('reporte-mes/', views.reporte_usuarios_mes, name='reporte-mes'),
+
+
+
+
+
+
 
 ]
--- WEB/reportes/views.py
+++ WEB/reportes/views.py
@@ -1,4 +1,8 @@
 # Por defecto
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from reportes.util import *
 import datetime
@@ -37,3 +41,105 @@ def show_users(request):
         args = {'title': 'CECEQ Reportes', 'users_anon': users_anon,'hoy':hoy}
         return render(request, 'reportes/usuarios.html', args)
     return redirect('login')
+
+
+def reporte_usuarios_hoy(request):
+    if request.user.is_authenticated:
+        hoy = time.strftime("%d/%m/%y")
+        hora = time.strftime("%I:%M:%S")
+        users_anon = get_anonimus_users()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=Hoy-reporte-usuarios.pdf'
+        buffer = BytesIO()
+        c = canvas.Canvas(buffer,pagesize=A4)
+
+        c.setLineWidth(.3)
+        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 735, 'Reporte de Usuarios de hoy:')
+        c.drawString(480, 750, hoy)
+        c.drawString(480, 735, hora)
+        c.line(460,747,560,747)
+        c.drawString(30,700,'El numero de usuarios hoy ha sido de:')
+        c.save()
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+        return response
+
+    return redirect('login')
+
+def reporte_usuarios_semana(request):
+    if request.user.is_authenticated:
+        hoy = time.strftime("%d/%m/%y")
+        hora = time.strftime("%I:%M:%S")
+        users_anon = get_anonimus_users()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=Semana-reporte-usuarios.pdf'
+        buffer = BytesIO()
+        c = canvas.Canvas(buffer,pagesize=A4)
+
+        c.setLineWidth(.3)
+        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 735, 'Reporte de Usuarios de esta semana:')
+        c.drawString(480, 750, hoy)
+        c.drawString(480, 735, hora)
+        c.line(460,747,560,747)
+        c.drawString(30,700,'El numero de usuarios esta semana ha sido de:')
+        c.save()
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+        return response
+
+    return redirect('login')
+
+def reporte_usuarios_mes(request):
+    if request.user.is_authenticated:
+        hoy = time.strftime("%d/%m/%y")
+        hora = time.strftime("%I:%M:%S")
+        users_anon = get_anonimus_users()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=Mes-reporte-usuarios.pdf'
+        buffer = BytesIO()
+        c = canvas.Canvas(buffer,pagesize=A4)
+
+        c.setLineWidth(.3)
+        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 735, 'Reporte de Usuarios de este mes:')
+        c.drawString(480, 750, hoy)
+        c.drawString(480, 735, hora)
+        c.line(460,747,560,747)
+        c.drawString(30,700,'El numero de usuarios este mes ha sido de:')
+        c.save()
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+        return response
+
+    return redirect('login')
+
+def reporte_usuarios_año(request):
+    if request.user.is_authenticated:
+        hoy = time.strftime("%d/%m/%y")
+        hora = time.strftime("%I:%M:%S")
+        users_anon = get_anonimus_users()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=Año-reporte-usuarios.pdf'
+        buffer = BytesIO()
+        c = canvas.Canvas(buffer,pagesize=A4)
+
+        c.setLineWidth(.3)
+        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 735, 'Reporte de Usuarios de este año:')
+        c.drawString(480, 750, hoy)
+        c.drawString(480, 735, hora)
+        c.line(460,747,560,747)
+        c.drawString(30,700,'El numero de usuarios este año ha sido de:')
+        c.save()
+        pdf = buffer.getvalue()
+        buffer.close()
+        response.write(pdf)
+        return response
+
+    return redirect('login')
+
