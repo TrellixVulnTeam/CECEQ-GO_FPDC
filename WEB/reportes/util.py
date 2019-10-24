8c97bd404d87c7d9bc207f0b30e7c0c6c267c7c2
Jose Carlos <a01702828@itesm.mx>
2019-10-24 01:08:35 -0500

    Parte de reportes terminado

--- WEB/reportes/util.py
+++ WEB/reportes/util.py
@@ -1,7 +1,7 @@
 from usuarios.models import *
 from reportes.models import *
 import time
-from datetime import date
+from datetime import *
 def get_anonimus_users():
     users_r = UsuariosAnonimos.objects.all()
     list_of_ids = []
@@ -28,7 +28,7 @@ def get_cursos():
 
 def get_anonimus_users_year():
     today  = date.today()
-    users_r = UsuariosAnonimos.objects.filter(time__year =today.year)
+    users_r = UsuariosAnonimos.objects.filter(time__year = today.year)
     list_of_ids = []
     number = 0
     for user_r in users_r:
@@ -38,7 +38,7 @@ def get_anonimus_users_year():
 
 def get_anonimus_users_today():
     today  = date.today()
-    users_r = UsuariosAnonimos.objects.filter(time__year =today.year,time__month=today.month,time__day=today.day)
+    users_r = UsuariosAnonimos.objects.filter(time = today)
     list_of_ids = []
     number = 0
     for user_r in users_r:
@@ -46,14 +46,148 @@ def get_anonimus_users_today():
         list_of_ids.append(user_r.user_id)
     return number
 
-def get_anonimus_users_month():
-    today  = date.today()
-    users_r = UsuariosAnonimos.objects.filter(time__month=today.month)
+def get_anonimus_users_mes():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = mes)
     list_of_ids = []
     number = 0
     for user_r in users_r:
         number = number + 1
         list_of_ids.append(user_r.user_id)
     return number
+
+#----------------------------filtro cada mes---------------
+
+def get_anonimus_users_enero():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = mes)
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_1():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '1')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_2():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '2')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_3():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '3')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_4():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '4')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_5():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '5')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_6():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '6')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_7():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '7')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_8():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '8')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_9():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '9')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_10():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '10')
+    list_of_ids = []
+    number = 3
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_11():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '11')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+def get_anonimus_users_12():
+    mes = datetime.now().month
+    users_r = UsuariosAnonimos.objects.filter(time__month = '12')
+    list_of_ids = []
+    number = 1
+    for user_r in users_r:
+        number = number + 1
+        list_of_ids.append(user_r.user_id)
+    return number
+
+
 
 
--- WEB/reportes/views.py
+++ WEB/reportes/views.py
@@ -7,32 +7,50 @@ from django.shortcuts import render, redirect
 from reportes.util import *
 import datetime
 import time
-#URLS de Reportes
+
+
+# URLS de Reportes
 
 def cursos(request):
     if request.user.is_authenticated:
         eventos = get_cursos()
-        args = {'eventos':eventos}
-        return render(request, 'reportes/cursos.html',args)
+        args = {'eventos': eventos}
+        return render(request, 'reportes/cursos.html', args)
     return redirect('login')
+
 
 def salones(request):
     if request.user.is_authenticated:
-
         return render(request, 'reportes/salones.html')
     return redirect('login')
+
 
 def usuarios(request):
     if request.user.is_authenticated:
         return render(request, 'reportes/usuarios.html')
     return redirect('login')
 
+
 def visitas(request):
     if request.user.is_authenticated:
         users_anon = get_anonimus_users_today()
         user_year = get_anonimus_users_year()
-        args = {'user_anon':users_anon,'user_year':user_year}
-        return render(request, 'reportes/visitas.html',args)
+        u1 = get_anonimus_users_1()
+        u2 = get_anonimus_users_2()
+        u3 = get_anonimus_users_3()
+        u4 = get_anonimus_users_4()
+        u5 = get_anonimus_users_5()
+        u6 = get_anonimus_users_6()
+        u7 = get_anonimus_users_7()
+        u8 = get_anonimus_users_8()
+        u9 = get_anonimus_users_9()
+        u10 = get_anonimus_users_10()
+        u11 = get_anonimus_users_11()
+        u12 = get_anonimus_users_12()
+
+        args = {'user_anon': users_anon, 'user_year': user_year, 'u1': u1, 'u2': u2, 'u3': u3, 'u4': u4, 'u5': u5,
+                'u6': u6, 'u7': u7, 'u8': u8, 'u9': u9, 'u10': u10, 'u11': u11, 'u12': u12}
+        return render(request, 'reportes/visitas.html', args)
     return redirect('login')
 
 
@@ -40,7 +58,7 @@ def show_users(request):
     if request.user.is_authenticated:
         hoy = time.strftime("%I:%M:%S")
         users_anon = get_anonimus_users()
-        args = {'title': 'CECEQ Reportes', 'users_anon': users_anon,'hoy':hoy}
+        args = {'title': 'CECEQ Reportes', 'users_anon': users_anon, 'hoy': hoy}
         return render(request, 'reportes/usuarios.html', args)
     return redirect('login')
 
@@ -49,19 +67,19 @@ def reporte_usuarios_hoy(request):
     if request.user.is_authenticated:
         hoy = time.strftime("%d/%m/%y")
         hora = time.strftime("%I:%M:%S")
-        users_anon = get_anonimus_users()
+        users_anon = get_anonimus_users_today()
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=Hoy-reporte-usuarios.pdf'
         buffer = BytesIO()
-        c = canvas.Canvas(buffer,pagesize=A4)
+        c = canvas.Canvas(buffer, pagesize=A4)
 
         c.setLineWidth(.3)
-        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 750, 'CECEQ -GO')
         c.drawString(30, 735, 'Reporte de Usuarios de hoy:')
         c.drawString(480, 750, hoy)
         c.drawString(480, 735, hora)
-        c.line(460,747,560,747)
-        c.drawString(30,700,'El numero de usuarios hoy ha sido de:')
+        c.line(460, 747, 560, 747)
+        c.drawString(30, 700, 'El numero de usuarios hoy ha sido de:'+ str(users_anon))
 
         c.save()
         pdf = buffer.getvalue()
@@ -70,6 +88,7 @@ def reporte_usuarios_hoy(request):
         return response
 
     return redirect('login')
+
 
 def reporte_usuarios_semana(request):
     if request.user.is_authenticated:
@@ -79,15 +98,15 @@ def reporte_usuarios_semana(request):
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=Semana-reporte-usuarios.pdf'
         buffer = BytesIO()
-        c = canvas.Canvas(buffer,pagesize=A4)
+        c = canvas.Canvas(buffer, pagesize=A4)
 
         c.setLineWidth(.3)
-        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 750, 'CECEQ -GO')
         c.drawString(30, 735, 'Reporte de Usuarios de esta semana:')
         c.drawString(480, 750, hoy)
         c.drawString(480, 735, hora)
-        c.line(460,747,560,747)
-        c.drawString(30,700,'El numero de usuarios esta semana ha sido de:')
+        c.line(460, 747, 560, 747)
+        c.drawString(30, 700, 'El numero de usuarios esta semana ha sido de:')
         c.save()
         pdf = buffer.getvalue()
         buffer.close()
@@ -95,6 +114,7 @@ def reporte_usuarios_semana(request):
         return response
 
     return redirect('login')
+
 
 def reporte_usuarios_mes(request):
     if request.user.is_authenticated:
@@ -104,15 +124,15 @@ def reporte_usuarios_mes(request):
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=Mes-reporte-usuarios.pdf'
         buffer = BytesIO()
-        c = canvas.Canvas(buffer,pagesize=A4)
+        c = canvas.Canvas(buffer, pagesize=A4)
 
         c.setLineWidth(.3)
-        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 750, 'CECEQ -GO')
         c.drawString(30, 735, 'Reporte de Usuarios de este mes:')
         c.drawString(480, 750, hoy)
         c.drawString(480, 735, hora)
-        c.line(460,747,560,747)
-        c.drawString(30,700,'El numero de usuarios este mes ha sido de:')
+        c.line(460, 747, 560, 747)
+        c.drawString(30, 700, 'El numero de usuarios este mes ha sido de:')
         c.save()
         pdf = buffer.getvalue()
         buffer.close()
@@ -120,24 +140,25 @@ def reporte_usuarios_mes(request):
         return response
 
     return redirect('login')
+
 
 def reporte_usuarios_año(request):
     if request.user.is_authenticated:
         hoy = time.strftime("%d/%m/%y")
         hora = time.strftime("%I:%M:%S")
-        users_anon = get_anonimus_users()
+        users_anon = get_anonimus_users_year()
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename=Año-reporte-usuarios.pdf'
         buffer = BytesIO()
-        c = canvas.Canvas(buffer,pagesize=A4)
+        c = canvas.Canvas(buffer, pagesize=A4)
 
         c.setLineWidth(.3)
-        c.drawString(30,750,'CECEQ -GO')
+        c.drawString(30, 750, 'CECEQ -GO')
         c.drawString(30, 735, 'Reporte de Usuarios de este año:')
         c.drawString(480, 750, hoy)
         c.drawString(480, 735, hora)
-        c.line(460,747,560,747)
-        c.drawString(30,700,'El numero de usuarios este año ha sido de:')
+        c.line(460, 747, 560, 747)
+        c.drawString(30, 700, 'El numero de usuarios este año ha sido de:' + str(users_anon))
         c.save()
         pdf = buffer.getvalue()
         buffer.close()
@@ -145,4 +166,3 @@ def reporte_usuarios_año(request):
         return response
 
     return redirect('login')
-
