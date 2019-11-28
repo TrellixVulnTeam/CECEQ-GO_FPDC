971708a95add515b89a86994126855946e704e25
gimape07 <a01702312@itesm.mx>
2019-11-28 05:17:11 -0600

    Final version

--- WEB/mapas/views.py
+++ WEB/mapas/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from reportes.util import *
+from django.contrib.staticfiles.templatetags.staticfiles import *
+from django.template.defaulttags import register
+from django.core.files.storage import default_storage
+from django.contrib.staticfiles import finders
+from django.conf import settings
 
 def mapasA(request):
     if request.user.is_authenticated:
@@ -14,3 +19,8 @@ def mapasB(request):
     if request.user.is_authenticated:
         return render(request, 'mapas/mapasB.html')
     return redirect('login')
+
+
+@register.filter(name='image_exists')
+def image_exists(image):
+    return default_storage.exists(settings.STATIC_ROOT+image)
